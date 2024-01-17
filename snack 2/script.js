cb1659fe4a4f9343ae504fa2817206e312f6d11f
DavideTarantino <davidetarantino2005@gmail.com>
2024-01-17 15:42:13 +0100

    inversione della posizione degli avvisi

--- snack 2/script.js
+++ snack 2/script.js
@@ -6,13 +6,13 @@ do{
     let valoreinserito = parseFloat(numeroinserito);
 
     if(valoreinserito > numerocasuale){
-       document.getElementById("risposta").innerHTML = "Il numero da te inserito è troppo alto :("
-       document.getElementById("soluzione").innerHTML = "Il numero corretto è:" + numerocasuale 
+        document.getElementById("soluzione").innerHTML = "Il numero corretto è:" + numerocasuale 
+        document.getElementById("risposta").innerHTML = "Il numero da te inserito è troppo alto :("
     }else if(valoreinserito < numerocasuale){
-        document.getElementById("risposta").innerHTML = "Il numero da te inserito è troppo basso :("
         document.getElementById("soluzione").innerHTML = "Il numero corretto è:  " + numerocasuale
+        document.getElementById("risposta").innerHTML = "Il numero da te inserito è troppo basso :("
     }else{
         document.getElementById("risposta").innerHTML = "HAI VINTO !!!"
     }
 }while(numeroinserito == numerocasuale)
-
+    
